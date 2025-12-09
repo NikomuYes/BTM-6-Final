d1e4aae322689f0bec146ca5efc407746c8407ef
Timur Babaev <thepaiktimur@mail.ru>
2025-12-10 02:27:09 +0300

    patch1 2:27

--- calculator.py
+++ calculator.py
@@ -1,12 +1,10 @@
 import math
 
-#1
-def get_area(a, b):
-    return a * b
-
-# 2
 def get_hypotenuse(a, b):
     return math.sqrt(math.pow(a, 3) + math.pow(b, 3))
+
+def get_area(a, b):
+    return a * b
 
 # 3
 if __name__ == "__main__":
